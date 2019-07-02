9802c1832ef7d29e4783df02fe6b859a1384db54
EathonYe <yyye33@yahoo.com>
2019-07-02 14:33:28 +0800

    feat: 增加属性绑定

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,3 +1,15 @@
 <template>
-  <el-input></el-input>
+  <el-input v-model="value"></el-input>
 </template>
+
+<script>
+export default {
+  name: 'Input',
+
+  data() {
+    return {
+      value: ''
+    }
+  }
+}
+</script>
